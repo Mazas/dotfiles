2e0e0bc27661f19271ea7162fa1a4a6212c48459
Tadas Ivanauskas <ivanauskas15@gmail.com>
2022-01-01 09:01:40 +0100

    Update and rename rss.sh to rss.py

---
+++ conky/rss.py
@@ -0,0 +1,13 @@
+import feedparser
+
+feed = feedparser.parse("https://hnrss.org/frontpage")
+
+size = len(feed.entries) if len(feed.entries) <= 20 else 20
+
+output = ''
+
+for i in range(0, size):
+    prefix = '├─ {} '.format(i+1) if i < size - 1 else '└─ {} '.format(i+1)
+    output =  output + prefix + str(feed.entries[i].title) + '\n'
+
+print(output)
